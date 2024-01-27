41cd935f967a6e7914eb9b057baa124e42dc4a53
wannabefrontdeveloper <giwonk@naver.com>
2024-01-27 20:37:23 +0900

    코드수정 checkLoggedIn 미들웨어를 적용하였습니다.

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -1,15 +1,16 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
 const post = new Router();
 posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+posts.delete('/:id', checkLoggedIn, postsCtrl.remove);
+posts.patch('/:id', checkLoggedIn, postsCtrl.update);
 
 posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
