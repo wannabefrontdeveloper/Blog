ea4c7d4ea46d29527575a3057962b2f8a93aae57
wannabefrontdeveloper <giwonk@naver.com>
2024-01-27 18:27:42 +0900

    코드 수정 /api/posts/:id 경로를 위한 라우터를 새로 만들고, posts에 해당 라우터를 등록해주었습니다.

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -5,8 +5,12 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+
+const post = new Router();
+posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
