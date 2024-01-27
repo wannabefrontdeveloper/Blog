ca0547234f7a85cc52ed0df03ab88577eed9c6c1
wannabefrontdeveloper <giwonk@naver.com>
2024-01-27 17:46:18 +0900

    기능 추가 업데이트 기능을 추가하였습니다.

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -48,4 +48,19 @@ export const remove = async (ctx) => {
   }
 };
 
-export const update = (ctx) => {};
+export const update = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+      // false일때는 업데이트 되기 전의 데이터를 반환합니다.
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
