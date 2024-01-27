f61a92731e5b0a7b7f0fc63a2b70e7b0f8067b36
wannabefrontdeveloper <giwonk@naver.com>
2024-01-27 20:13:10 +0900

    코드 수정 미들웨어를 적용하였습니다.

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -5,7 +5,7 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import api from './api';
-import createFakeData from './createFakeData';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -13,7 +13,6 @@ mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    createFakeData();
   })
   .catch((e) => {
     console.error(e);
@@ -25,6 +24,7 @@ const router = new Router();
 router.use('/api', api.routes());
 
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 app.use(router.routes()).use(router.allowedMethods());
 
