6a24b57765da7e3be0d002097111a4888df16573
wannabefrontdeveloper <giwonk@naver.com>
2024-01-28 13:16:48 +0900

    코드수정 type 값에 따라 사용되는 문구도 달라지고, type이 'register'일 때는 비밀번호 확인 인풋도 보여주도록하였습니다.

--- blog-frontend/src/components/auth/AuthForm.js
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -50,10 +50,15 @@ const Footer = styled.div`
 const ButtonWithMarginTop = styled(Button)`
   margin-top: 1rem;
 `
-const AuthForm = () => {
+const textMap = {
+  login:'로그인',
+  register:'회원가입',
+}
+const AuthForm = ({type}) => {
+  const text = textMap[type];
   return (
     <AuthFormBlock>
-      <h3>로그인</h3>
+      <h3>{text}</h3>
       <form>
         <StyledInput autoComplete="username" name="username" placeholder="아이디" />
         <StyledInput
@@ -62,10 +67,25 @@ const AuthForm = () => {
           placeholder="비밀번호"
           type="password"
         />
-        <ButtonWithMarginTop cyan fullWidth >로그인</ButtonWithMarginTop>
+        {type === 'register' && (
+          <StyledInput
+          autoComplete="new-password"
+          name="passwordConfirm"
+          placeholder="비밀번호 확인"
+          type="password"
+          />
+        )}
+        <ButtonWithMarginTop cyan fullWidth style={{marginTop: '1rem'}}>
+          {text}
+          </ButtonWithMarginTop>
+        
       </form>
       <Footer>
-        <Link to="/register">회원가입</Link>
+        {type === 'login' ? (
+          <Link to="/register">회원가입</Link>
+        ) : (
+          <Link to="/login">로그인</Link>
+        )}
       </Footer>
     </AuthFormBlock>
   );
