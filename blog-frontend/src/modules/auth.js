1ac666a72275850e4ad751738edb9faf69a6e3f3
wannabefrontdeveloper <giwonk@naver.com>
2024-01-28 13:26:32 +0900

    코드수정 리덕스로 회원가입과 로그인 폼의 상태를 관리하도록 하였습니다.

--- blog-frontend/src/modules/auth.js
+++ blog-frontend/src/modules/auth.js
@@ -1,14 +1,42 @@
 import { createAction, handleActions } from 'redux-actions';
+import { produce } from 'immer'; // immer에서 직접 produce를 import
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const CHANGE_FIELD = 'auth/CHANGED_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const sampleAction = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }) => ({
+    form,
+    key,
+    value,
+  }),
+);
 
-const initialState = {};
+export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
+
+const initialState = {
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  login: {
+    username: '',
+    password: '',
+  },
+};
 
 const auth = handleActions(
   {
-    [SAMPLE_ACTION]: (state, action) => state,
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, (draft) => {
+        draft[form][key] = value;
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+    }),
   },
   initialState,
 );
