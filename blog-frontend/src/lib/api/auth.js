17c0d2e01fbc7cb7dc1e40468d950e17deeaa321
wannabefrontdeveloper <giwonk@naver.com>
2024-01-30 10:00:11 +0900

    코드 추가 로그아웃 함수를 추가하였습니다.

--- blog-frontend/src/lib/api/auth.js
+++ blog-frontend/src/lib/api/auth.js
@@ -10,3 +10,6 @@ client.post('/api/auth/register',{ username, password });
 
 // 로그인 상태 확인
 export const check = () => client.get('/api/auth/check');
+
+// 로그아웃
+export const logout = () => client.post('api/auth/logout');
