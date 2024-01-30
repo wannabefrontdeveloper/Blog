7e553d45458594ae7b2f00728da877bcecaf0588
wannabefrontdeveloper <giwonk@naver.com>
2024-01-30 10:04:09 +0900

    코드추가 LOGOUT이라는 액션을 만들고 이 액션이 디스패치되었을때 API 호출 후 localStorage의 user 값을 지워주도록 하였습니다.

--- blog-frontend/src/modules/user.js
+++ blog-frontend/src/modules/user.js
@@ -1,5 +1,5 @@
 import {createAction, handleActions} from 'redux-actions';
-import {takeLatest} from 'redux-saga/effects';
+import {takeLatest, call} from 'redux-saga/effects';
 import * as authAPI from '../lib/api/auth';
 import createRequestSaga, {
   createRequestActionTypes,
@@ -11,8 +11,11 @@ const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
   'user/CHECK',
 )
 
+const LOGOUT = 'user/LOGOUT';
+
 export const tempSetUser = createAction(TEMP_SET_USER, user=> user);
 export const check = createAction(CHECK);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
 
@@ -24,9 +27,19 @@ function checkFailureSaga() {
   }
 }
 
+function * logoutSaga() {
+  try {
+    yield call(authAPI.logout); // logout API 호출
+    localStorage.removeItem('user'); // localSotrage에서 user를 제거
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
   yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+  yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initialState = {
@@ -49,6 +62,10 @@ export default handleActions(
       ...state,
       user:null,
       checkError: error,
+    }),
+    [LOGOUT]: state => ({
+      ...state,
+      user:null,
     }),
   },
   initialState,
