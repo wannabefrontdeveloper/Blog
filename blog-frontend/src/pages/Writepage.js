d9d3377bfd7a8388161cd4d5802ad96c52e494c5
wannabefrontdeveloper <giwonk@naver.com>
2024-01-31 10:37:18 +0900

    코드수정 Editor 컴포넌트를 WritePage에 렌더링하였습니다.

--- blog-frontend/src/pages/Writepage.js
+++ blog-frontend/src/pages/Writepage.js
@@ -1,5 +1,12 @@
+import Editor from "../components/write/Editor";
+import Responsive from "../components/common/Responsive";
+
 const WritePage = () => {
-  return <div>글쓰기</div>;
-};
+  return (
+    <Responsive>
+      <Editor />
+    </Responsive>
+  )
+}
 
 export default WritePage;
