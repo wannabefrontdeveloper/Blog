5459e5e6a0dc193832d01f4df1ccaad468a92bfa
wannabefrontdeveloper <giwonk@naver.com>
2024-02-01 16:58:24 +0900

    오타수정

--- blog-frontend/src/containers/posts/PaginationContainer.js
+++ blog-frontend/src/containers/posts/PaginationContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Pagination from '../../components/posts/Pagination';
-import { UseSelector, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import {useParams, useSearchParams} from 'react-router-dom';
 
 const PaginationContainer = () => {
