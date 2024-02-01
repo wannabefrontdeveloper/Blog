cf384b41d38cf02ccc4dd58ab16c896a6153e3c7
wannabefrontdeveloper <giwonk@naver.com>
2024-02-01 10:46:25 +0900

    코드수정 PostList 컴포넌트를 렌더링하였습니다.

--- blog-frontend/src/pages/PostListPage.js
+++ blog-frontend/src/pages/PostListPage.js
@@ -1,10 +1,11 @@
 import HeaderContainer from "../components/common/HeaderContainer";
+import PostList from "../components/posts/PostList";
 
 const PostListPage = () => {
   return (
     <>
     <HeaderContainer />
-    <div>안녕하세요</div>
+    <PostList />
     </>
   );
 };
