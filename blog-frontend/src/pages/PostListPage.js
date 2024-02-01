0a248b57ccc08cb6cae44eac4dc1afae92ea0235
wannabefrontdeveloper <giwonk@naver.com>
2024-02-01 17:05:00 +0900

    코드추가 PaginationContainer 컴포넌트를 렌더링해주었습니다.

--- blog-frontend/src/pages/PostListPage.js
+++ blog-frontend/src/pages/PostListPage.js
@@ -1,11 +1,13 @@
 import HeaderContainer from "../components/common/HeaderContainer";
 import PostListContainer from "../containers/posts/PostListContainer";
+import PaginationContainer from "../containers/posts/PaginationContainer";
 
 const PostListPage = () => {
   return (
     <>
     <HeaderContainer />
     <PostListContainer />
+    <PaginationContainer />
     </>
   );
 };
