d6755d2645052870f13bad4b51a1626e681768a3
wannabefrontdeveloper <giwonk@naver.com>
2024-02-01 22:33:23 +0900

    코드추가 선언문 추가

--- blog-frontend/src/pages/PostListPage.js
+++ blog-frontend/src/pages/PostListPage.js
@@ -1,13 +1,14 @@
-import HeaderContainer from "../components/common/HeaderContainer";
-import PostListContainer from "../containers/posts/PostListContainer";
-import PaginationContainer from "../containers/posts/PaginationContainer";
+import React from 'react';
+import HeaderContainer from '../components/common/HeaderContainer';
+import PostListContainer from '../containers/posts/PostListContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
 
 const PostListPage = () => {
   return (
     <>
-    <HeaderContainer />
-    <PostListContainer />
-    <PaginationContainer />
+      <HeaderContainer />
+      <PostListContainer />
+      <PaginationContainer />
     </>
   );
 };
