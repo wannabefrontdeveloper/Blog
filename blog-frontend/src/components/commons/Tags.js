89267b151e64af7db8a32503523304d3bb992bf6
wannabefrontdeveloper <giwonk@naver.com>
2024-02-02 17:12:21 +0900

    오류해결

--- blog-frontend/src/components/commons/Tags.js
+++ blog-frontend/src/components/commons/Tags.js
@@ -1,14 +1,13 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 
-
-const TagsBlock = styled.div `
+const TagsBlock = styled.div`
   margin-top: 0.5rem;
   .tag {
     display: inline-block;
     color: ${palette.cyan[7]};
-    text-decoration:none;
+    text-decoration: none;
     margin-right: 0.5rem;
     &:hover {
       color: ${palette.cyan[6]};
@@ -16,16 +15,18 @@ const TagsBlock = styled.div `
   }
 `;
 
-const Tags = ({tags}) => {
+const Tags = ({ tags }) => {
+  console.log("Tags - tags:", tags);
+
   return (
     <TagsBlock>
-      {tags.map(tag => (
-        <Link className = "tag" to={`/?tag=${tag}`} key={tag}>
+      {tags && tags.map(tag => (
+        <Link className="tag" to={`/?tag=${tag}`} key={tag}>
           #{tag}
         </Link>
       ))}
     </TagsBlock>
-  )
-}
+  );
+};
 
 export default Tags;
