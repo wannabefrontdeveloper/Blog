1f4379264203299b407577a1b25ededcd7fbc9a8
wannabefrontdeveloper <giwonk@naver.com>
2024-02-02 12:29:25 +0900

    코드수정 포스트 작성 API 대신 수정 API를 사용하는 기능을 구현하였습니다.

--- blog-frontend/src/lib/api/posts.js
+++ blog-frontend/src/lib/api/posts.js
@@ -10,3 +10,10 @@ export const listPosts = ({page, username, tag}) => {
     params: {page, username, tag},
   })
 }
+
+export const updatePost = ({id, title, body, tags}) => 
+client.patch(`/api/posts/${id}`, {
+  title,
+  body,
+  tags,
+})
