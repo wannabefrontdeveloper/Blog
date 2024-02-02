2ef1d36e22bac69508fa89d82090948090147e5f
wannabefrontdeveloper <giwonk@naver.com>
2024-02-02 16:06:37 +0900

    코드수정 Helmet 컴포넌트를 사용하였습니다.

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -4,19 +4,20 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/Writepage';
 import PostPage from './pages/PostPage';
+import {Helmet} from 'react-helmet-async';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<PostListPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/write" element={<WritePage />} />
-      <Route path="/@:username">
-        <Route index element={<PostListPage />} />
-        <Route path=":postId" element={<PostPage />} />
-      </Route>
-    </Routes>
+    <>
+    <Helmet>
+      <title>REACTERS</title>
+    </Helmet>
+   <Route component = {PostListPage} path={['/@:username','/']} exact />
+   <Route component = {LoginPage} path="/login" />
+   <Route component = {RegisterPage} path="/register" />
+   <Route component = {WritePage} path="/write" />
+   <Route component = {PostPage} path="/@:username/:postId" />
+    </>
   );
 };
 
