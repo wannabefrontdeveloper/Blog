b0505bddc99ad31b195a951dfb755d5e62b6ed58
wannabefrontdeveloper <giwonk@naver.com>
2024-02-02 12:23:35 +0900

    코드수정 현재 사용자가 보고 있는 포스트가 자신의 포스트일 때만 PostActionButtons가 나타나게 설정하였습니다.

--- blog-frontend/src/containers/post/PostViewerContainer.js
+++ blog-frontend/src/containers/post/PostViewerContainer.js
@@ -1,18 +1,22 @@
 import React, {useEffect} from 'react';
 import { useDispatch,useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
 import {readPost, unloadPost} from '../../modules/post';
 import PostViewer from '../../components/post/postViewer';
 import PostActionButtons from '../../components/post/PostActionButtons';
+import { setOriginalPost } from '../../modules/write';
+import {removePost} from '../../lib/api/posts';
+import { useParams,useNavigate } from 'react-router-dom';
 
 const PostViewerContainer = () => {
   // 처음 마운트될때 포스트 읽기 API 요청
   const {postId} = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const {post,error,loading} = useSelector(({post,loading}) => ({
+  const {post,error,loading, user} = useSelector(({post,loading,user}) => ({
     post: post.post,
     error: post.error,
     loading: loading['post/READ_POST'],
+    user:user.user,
   }))
 
   useEffect(() => {
@@ -22,8 +26,22 @@ const PostViewerContainer = () => {
       dispatch(unloadPost());
     }
   },[dispatch, postId]);
+  
+  const onEdit = () => {
+  dispatch(setOriginalPost(post));
+  navigate('/write');
+  }
 
-  return <PostViewer post = {post} loading={loading} error={error} actionButtons={<PostActionButtons/>}/>;
+  const ownPost = (user && user._id) === (post && post.user._id);
+
+  return <PostViewer 
+  post = {post} 
+  loading={loading} 
+  error={error} 
+  actionButtons={
+    ownPost && <PostActionButtons onEdit={onEdit}/>
+}
+/>;
 }
 
 export default PostViewerContainer;
