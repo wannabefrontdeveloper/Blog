7ab7871d538dbb56343b42263fe180e92bb95f86
wannabefrontdeveloper <giwonk@naver.com>
2024-02-02 16:22:12 +0900

    코드수정 리액트라우터 버전에 맞게 코드를 수정하였습니다.

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,22 +1,25 @@
-import { Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/Writepage';
 import PostPage from './pages/PostPage';
-import {Helmet} from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async';
 
 const App = () => {
   return (
     <>
-    <Helmet>
-      <title>REACTERS</title>
-    </Helmet>
-   <Route component = {PostListPage} path={['/@:username','/']} exact />
-   <Route component = {LoginPage} path="/login" />
-   <Route component = {RegisterPage} path="/register" />
-   <Route component = {WritePage} path="/write" />
-   <Route component = {PostPage} path="/@:username/:postId" />
+      <Helmet>
+        <title>REACTERS</title>
+      </Helmet>
+      <Routes>
+        <Route path="/@:username" element={<PostListPage />} />
+        <Route path="/" element={<PostListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/@:username/:postId" element={<PostPage />} />
+      </Routes>
     </>
   );
 };
