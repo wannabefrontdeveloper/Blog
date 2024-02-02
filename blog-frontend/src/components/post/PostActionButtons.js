553c6ed66713e581a0ce7aeaea6bd55eed03c0cc
wannabefrontdeveloper <giwonk@naver.com>
2024-02-02 12:11:45 +0900

    코드생성 포스트 작성자에게만 포스트 상단에 수정 버튼과 삭제 버튼이 나타나도록 하였습니다.

---
+++ blog-frontend/src/components/post/PostActionButtons.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+
+const PostActionButtonBlock = styled.div `
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 2rem;
+  margin-top: -1.5rem;
+`;
+
+const ActionButton = styled.button `
+  padding: 0.25rem 0.5rem;
+  border-radius: 4px;
+  color: ${palette.gray[6]};
+  font-weight: bold;
+  border:none;
+  outline: none;
+  font-size: 0.875rem;
+  &:hover {
+    background: ${palette.gray[1]};
+    color: ${palette.cyan[7]};
+  }
+  & + & {
+    margin-left: 0.25rem;
+  }
+`;
+
+const PostActionButtons = () => {
+  return (
+    <PostActionButtonBlock>
+      <ActionButton>수정</ActionButton>
+      <ActionButton>삭제</ActionButton>
+    </PostActionButtonBlock>
+  )
+}
+
+export default PostActionButtons;
