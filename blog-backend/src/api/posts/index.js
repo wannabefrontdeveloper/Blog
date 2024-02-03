c13a0d3c47cdd0fe6f07648f23d74531bb8352e6
wannabefrontdeveloper <giwonk@naver.com>
2024-02-03 20:20:42 +0900

    오타수정

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -8,9 +8,9 @@ posts.get('/', postsCtrl.list);
 posts.post('/', checkLoggedIn, postsCtrl.write);
 
 const post = new Router(); // /api/posts/:id
-posts.get('/', postsCtrl.read);
-posts.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
-posts.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
+post.get('/', postsCtrl.read);
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
 
 posts.use('/:id', postsCtrl.getPostById, post.routes());
 
