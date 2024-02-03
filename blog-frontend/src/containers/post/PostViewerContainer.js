a394e32983b42a294cf111dc97058bd2c9b6511f
wannabefrontdeveloper <giwonk@naver.com>
2024-02-03 20:34:50 +0900

    오타수정

--- blog-frontend/src/containers/post/PostViewerContainer.js
+++ blog-frontend/src/containers/post/PostViewerContainer.js
@@ -21,36 +21,37 @@ const PostViewerContainer = () => {
 
   useEffect(() => {
     dispatch(readPost(postId));
-    // 언마운트될 떄 리덕스에서 포스트 데이터 없애기
+    // 언마운트될 때 리덕스에서 포스트 데이터 없애기
     return () => {
       dispatch(unloadPost());
     }
   },[dispatch, postId]);
-  
+
   const onEdit = () => {
   dispatch(setOriginalPost(post));
   navigate('/write');
   }
 
-const onRemove = async () => {
-  try {
-    await removePost(postId);
-    navigate('/'); // 홈으로 이동
-  } catch (e) {
-    console.log(e);
+  const onRemove = async () => {
+    try {
+      await removePost(postId);
+      navigate('/'); // 홈으로 이동
+    } catch (e) {
+      console.log(e);
+    }
   }
-}
 
   const ownPost = (user && user._id) === (post && post.user._id);
 
-  return <PostViewer 
-  post = {post} 
-  loading={loading} 
-  error={error} 
-  actionButtons={
-    ownPost && <PostActionButtons onEdit={onEdit} onRemove={onRemove}/>
-}
-/>;
+  return(<PostViewer
+      post = {post}
+      loading={loading}
+      error={error}
+      actionButtons={
+        ownPost && <PostActionButtons onEdit={onEdit} onRemove={onRemove}/>
+      }
+    />
+  )
 }
 
 export default PostViewerContainer;
