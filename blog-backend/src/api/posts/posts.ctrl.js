64e8517074940d745bb8c17166778fed201e6bbb
wannabefrontdeveloper <giwonk@naver.com>
2024-02-03 21:02:03 +0900

    코드수정 몽고DB의 최신버전에 맞게 코드를 수정하였습니다.

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -146,7 +146,7 @@ export const read = async (ctx) => {
 export const remove = async (ctx) => {
   const { id } = ctx.params;
   try {
-    await Post.findByIdAndRemove(id).exec();
+    await Post.findByIdAndDelete(id).exec();
     ctx.status = 204; // No Content (성공은 했지만 응답할 데이터는 없음)
   } catch (e) {
     ctx.throw(500, e);
